339be8c18505eb33694f0f49ae3ab5c87fa1efd9
Harry Gogonis <hgogonis@gmail.com>
2015-04-29 21:02:41 -0400

    change file extension from .data to .csv

--- run_data.sh
+++ run_data.sh
@@ -2,17 +2,17 @@
 echo "====================================="
 echo "Iris Dataset:"
 echo "====================================="
-./LLS.py iris.data
+./LLS.py iris.csv
 echo "\n"
 
 echo "====================================="
 echo "Wine Dataset:"
 echo "====================================="
-./LLS.py wine.data --head
+./LLS.py wine.csv --head
 echo "\n"
 
 
 echo "====================================="
 echo "Parkisons Dataset:"
 echo "====================================="
-./LLS.py parkisons.csv 
+./LLS.py parkisons.csv
