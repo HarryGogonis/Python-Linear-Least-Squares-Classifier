4787f04a474edb7fa63d59115a59ea55dc07e2b6
Salimchaouqi <salimc@ufl.edu>
2015-08-31 11:36:59 -0400

    moved data files to separate folders, and fixed scripts for those data files

--- run_data.sh
+++ run_data.sh
@@ -7,23 +7,23 @@ LLS=LLS.py
 echo "====================================="
 echo "Iris Dataset:"
 echo "====================================="
-$SCRIPTPATH/$LLS iris.csv
+$SCRIPTPATH/$LLS data/iris.csv
 echo "\n"
 
 echo "====================================="
 echo "Wine Dataset:"
 echo "====================================="
-$SCRIPTPATH/$LLS wine.csv --head
+$SCRIPTPATH/$LLS data/wine.csv --head
 echo "\n"
 
 echo "====================================="
 echo "Parkinsons Dataset:"
 echo "====================================="
-$SCRIPTPATH/$LLS parkinson.csv
+$SCRIPTPATH/$LLS data/parkinson.csv
 echo "\n"
 
 echo "====================================="
 echo "Social Media Dataset:"
 echo "====================================="
-$SCRIPTPATH/$LLS social-media.csv
+$SCRIPTPATH/$LLS data/social-media.csv
 echo "\n"
